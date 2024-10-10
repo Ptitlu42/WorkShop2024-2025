2b23d4d9c54ea69b49fb5271c62bf112cf39443c
Ptitlu42 <lucas.beyer@gmx.fr>
2024-10-10 09:29:52 +0200

    save

--- js/main.js
+++ js/main.js
@@ -25,24 +25,6 @@ function fetch_informations() {
     });
 }
 
-function fetch_symptomes() {
-  fetch("../data/symptomes.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const symptomes = document.getElementById("symptomes");
-      data.forEach((symptome, index) => {
-        const li = document.createElement("li");
-        li.innerHTML = `
-          <input type="checkbox" id="symptome-${index}" name="${symptome.nom}" value="${symptome.nom}">
-          <label for="symptome-${index}">${symptome.nom}</label>
-        `;
-        symptomes.appendChild(li);
-      });
-
-      symptomes.addEventListener("change", updateLocalStorage);
-    });
-}
-
 function imprimerResultats() {
   window.print();
 }
@@ -59,6 +41,12 @@ function afficherSymptomesCoches() {
     p.textContent = symptome;
     symptomesCochesDiv.appendChild(p);
   });
+}
+
+function afficherCommentaire() {
+  const commentaireInput = document.getElementById("commentaire-patient");
+  const commentaire = localStorage.getItem("commentaire");
+  commentaireInput.value = commentaire;
 }
 
 function redirectTo(url) {
@@ -89,6 +77,23 @@ function recoverTaille() {
   localStorage.setItem("taille", taille);
 }
 
+function recoverCommentaire() {
+  const commentaireInput = document.getElementById("commentaire");
+  const commentaire = commentaireInput.value;
+  localStorage.setItem("commentaire", commentaire);
+}
+
+function getCommentaire() {
+  const commentaire = localStorage.getItem("commentaire");
+  document.getElementById("commentaire").innerHTML = commentaire;
+}
+
+function afficherCommentaire() {
+  const commentaire = localStorage.getItem("commentaire");
+  const commentaireElement = document.getElementById("commentaire-patient");
+  commentaireElement.innerHTML = commentaire;
+}
+
 function getTaille() {
   const taille = localStorage.getItem("taille");
   document.getElementById("taille").innerHTML = taille + " cm";
@@ -102,22 +107,22 @@ function calculerIMC() {
 }
 
 function randomPoids() {
-  const poids = Math.floor(Math.random() * (100 - 35 + 1)) + 35;
+  const poids = Math.floor(Math.random() * (90 - 45 + 1)) + 45;
   localStorage.setItem("poids", poids);
 }
 
 function randomBPM() {
-  const bpm = Math.floor(Math.random() * (250 - 20 + 1)) + 20;
+  const bpm = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
   localStorage.setItem("bpm", bpm);
 }
 
 function randomTemperature() {
-  const temperature = Math.floor(Math.random() * (45 - 32 + 1)) + 32;
+  const temperature = Math.floor(Math.random() * (40 - 36 + 1)) + 36;
   localStorage.setItem("temperature", temperature);
 }
 
 function randomGluco() {
-  const gluco = Math.floor(Math.random() * (300 - 40 + 1)) + 40;
+  const gluco = Math.floor(Math.random() * (220 - 90 + 1)) + 90;
   localStorage.setItem("gluco", gluco);
 }
 
@@ -131,12 +136,16 @@ function regrouperInformations() {
     gluco: localStorage.getItem("gluco"),
     sexe: document.getElementById("sexe").innerHTML,
     symptomes: [],
+    commentaire: localStorage.getItem("commentaire"),
+    age: calculerAge(document.getElementById("dateNaissance").innerHTML),
   };
+
 
   const checkedSymptomes = JSON.parse(localStorage.getItem("checkedSymptomes"));
   checkedSymptomes.forEach((symptome) => {
     informations.symptomes.push(symptome);
   });
+
 
   return informations;
 }
@@ -145,7 +154,8 @@ function diagnostic() {
   return new Promise((resolve, reject) => {
     const url = "https://api.openai.com/v1/chat/completions";
     const informations = regrouperInformations();
-    const apikey = "";
+    const apikey =
+      "";
 
     fetch(url, {
       method: "POST",
@@ -162,9 +172,10 @@ function diagnostic() {
               {
                 type: "text",
                 text: `
-                  "Je vais te fournir des informations médicales sur un patient. 
+                  "Je vais te fournir des informations médicales sur un patient, des valeurs, 
+                  son profil, ainsi qu'un commentaire de sa part. 
                   En fonction de ces informations, je souhaite que tu établisses un diagnostic 
-                  et que tu me proposes des suspicions de maladie ainsi que des conseils de traitement. 
+                  et que tu me proposes des suspicions de maladie ainsi que des conseils. 
                   Il est important de noter que je veux une réponse sous forme de chaîne de caractères pure, 
                   sans mise en forme en markdown. Et il faut que tu t'adresse au patient en le vouvoyant.
                 `,
@@ -205,7 +216,6 @@ function clarifierReponse(reponse) {
   return messageClarifie;
 }
 
-// Dans la page resultats.html
 function redirectToResultats() {
   const loaderElement = document.getElementById("loader");
   const resultatsElement = document.getElementById("resultats");
@@ -219,10 +229,62 @@ function redirectToResultats() {
   });
 }
 
-window.onload = function() {
+window.onload = function () {
   redirectToResultats();
-}
+};
 
 function redirectToListeResultats() {
   window.location.href = "../html/resultats.html";
+}
+
+function fetch_symptomes() {
+  fetch("../data/symptomes.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const symptomes = document.getElementById("symptomes");
+      data.forEach((symptome, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+          <input type="checkbox" id="symptome-${index}" name="${symptome.nom}" value="${symptome.nom}">
+          <label for="symptome-${index}">${symptome.nom}</label>
+        `;
+        symptomes.appendChild(li);
+      });
+
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", () => {
+          updateLocalStorage();
+        });
+      });
+
+      const searchInput = document.getElementById("search");
+      searchInput.addEventListener("input", (e) => {
+        const searchValue = e.target.value.toLowerCase();
+        const symptomesList = document.getElementById("symptomes");
+        const symptomesItems = symptomesList.children;
+
+        Array.from(symptomesItems).forEach((item) => {
+          const symptomeName = item
+            .querySelector("label")
+            .textContent.toLowerCase();
+          if (symptomeName.includes(searchValue)) {
+            item.style.display = "block";
+          } else {
+            item.style.display = "none";
+          }
+        });
+      });
+    });
+}
+
+function calculerAge(dateNaissance) {
+  const today = new Date();
+  const birthDate = new Date(dateNaissance);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
